3af59cfaf7ff48cbf4a04c28f32c64c770ef9cdf
Gilad Bechor <giladbechor@Gilads-MacBook-Pro.local>
2021-03-23 12:34:51 +0200

    Test

--- color_check/controllers/get_color_code.py
+++ color_check/controllers/get_color_code.py
@@ -6,11 +6,44 @@
 # for example by logging the request and error, and the user,
 # letting them know that their color doesn't exist.
 
+import json
+import logging
+
+
+# how do I get the color code from the dic and match it to 
+# user input?
+# how do i log the requests of valid and invalid colors?
+# how do i classify what a valid and invalid error is?
+# how do i implement errors if the color doesn't exist?
+
+
 def get_color_code(color_name):
+
+    if type(color_name) == str:
+        color_name = color_name.lower().strip()
+
+    try:
+        with open('./color_check/data/css-color-names.json') as f:
+            data = json.load(f)
+            color_hex_code = data[color_name]
+    
+    except KeyError:
+        logging.error('KeyError: {color_name}')
+        raise TypeError(f'KeyError: {color_name}')
+
+   
     # this is where you should add your logic to check the color.
     # Open the file at data/css-color-names.json, and return the hex code
     # The file can be considered as JSON format, or as a Python dictionary.
 
-    hex_code = '#0000ff'
 
-    return hex_code
+
+
+
+
+    # listElement = color_name[0]
+    # print (listElement)
+    # json_data = data
+    # hexcode = json_data[listElement]
+
+    # return hexcode
--- color_check/website.py
+++ color_check/website.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
+import time
+from datetime import datetime
 from color_check.controllers.get_color_code import get_color_code
 app = Flask(__name__)
 
@@ -9,7 +11,7 @@ def index():
     return render_template('index.html', page_title="Color Check")
 
 
-@app.route('/color', methods=['POST'])
+@app.route('/show_color/', methods=['POST'])
 def show_color():
     # When the user submits the form at /, the contents of the form
     # will be send to this route, and whatever code you write here will
@@ -21,12 +23,37 @@ def show_color():
     # - if the color doesn't exist, give the user a useful error message.
     # - create a log.txt file which records (logs) the user requests. 
 
-    user_submitted_string = 'blue'
-    color_hex_code = get_color_code(user_submitted_string)
+    
 
-    return render_template('color.html', page_title="Show Color",
-                           color_hex_code=color_hex_code)
+
+    req = request.form
+    inputcolor = req.get('color')
+
+
+    file = open("log.txt", "a") 
+    file.write("{} {} \n".format (datetime.utcfromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') , inputcolor)) 
+    file.close() 
+
+    try:
+        color_hex_code = get_color_code(inputcolor)
+        inputcolor = inputcolor.upper().strip()
+        return render_template('color.html', page_title="Show Color",
+                               color_hex_code=color_hex_code,
+                               color_name=inputcolor)
+        
+    except TypeError:
+        return render_template('wrongcolor.html', page_title="Color Not Found",
+                               color_name=inputcolor)
+        
+    
+
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=8080, debug=True)
+    app.run(host="localhost", port=8080, debug=True) 
+
+
+
+#logging the user inputs
+#log time of request
+
